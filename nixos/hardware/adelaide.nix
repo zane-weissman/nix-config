ccef48b191b50f35e8fa3e25ea9d29fd03c01dfa
Zane Weissman <zweissman@wpi.edu>
2024-08-22 18:34:25 -0400

    enableRedistributableFirmware = true for adelaide

--- nixos/hardware/adelaide.nix
+++ nixos/hardware/adelaide.nix
@@ -123,4 +123,7 @@
   # intel/x86
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  #?? driver fix maybe?
+  hardware.enableRedistributableFirmware = true;
 }
