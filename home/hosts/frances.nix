96532cc5eba1c732b7a0eda9be0f9b07445e1b5d
Zane Weissman <zweissman@wpi.edu>
2024-09-28 23:54:44 -0400

    move kmonad config to nixos (from home... whoops)

--- home/hosts/frances.nix
+++ home/hosts/frances.nix
@@ -8,56 +8,5 @@
 {
   #colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
   colorScheme = import ../colors/catppuccin-mocha-custom.nix;
-  
-  environment.systemPackages = with pkgs; [ kmonad ];
-  services.kmonad = {
-    enable = true;
-    keyboards = {
-      frameworkKeyboard = {
-        device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-        config = 
-          ''
-(defcfg
-  input  (device-file "/dev/input/by-id/usb-04d9_daskeyboard-event-kbd")
-  output (uinput-sink
-          "framework keyboard's kmonad out" ;; name of the created keyboard
-          "....") ;; additional, environment-specific, information
-  fallthrough true
-  allow-cmd false ;; maybe change later?
 
-  ;; aliases
-  
-  (defalias
-    agr (layer-toggle altgr)
-  )
-
-  ;; srcs and layers
-
-  (defsrc
-    esc   f1   f2   f3   f4   f5   f6   f7   f8   f9   f10   f11   f12   del
-    grb    1    2    3    4    5    6    7    8    9     0     -     =   bspc
-    tab    q    w    e    r    t    y    u    i    o     p     [     ]    \  
-    caps   a    s    d    f    g    h    j    k    l    ;      '       ret
-    lsft   z    x    c    v    b    n    m    ,    .    /     up  rsft  
-    lctl wkup lmet lalt      spc            ralt rctl  left  down right
-  )
-  (deflayer qwerty
-    esc   f1   f2   f3   f4   f5   f6   f7   f8   f9   f10   f11   f12   del
-    grb    1    2    3    4    5    6    7    8    9     0     -     =   bspc
-    tab    q    w    e    r    t    y    u    i    o     p     [     ]    \  
-    caps   a    s    d    f    g    h    j    k    l    ;      '       ret
-    lsft   z    x    c    v    b    n    m    ,    .    /     up  rsft  
-    lctl wkup lmet lalt      spc            agr rwin  left  down right
-  )
-  (deflayer altgr
-    -     -    -    -    -    -    -    -    -    -    -     -     -     -   
-    -      -    -    -    -    -    -    -    -    -     -     -     -   -   
-    -      -    -    -    -    -    -    home pgdn pgup  end   -     -    -   
-    -      -    -    -    -    -    -    left down up   right  -       -   
-    -      -    -    -    -    -    -    -    -    -    -     -   -     
-    -    -    -    -         -              -    -     -     -    -    
-  )
-)
-          '';
-  };
 }
--- nixos/hosts/frances.nix
+++ nixos/hosts/frances.nix
@@ -38,4 +38,59 @@
     #(builtins.readFile /persist/etc/ssl/certs/9f3cc5f891b54e947d422f8cca31345b8c3ec55a.cer)
     #(builtins.readFile /persist/etc/ssl/certs/zweissman.pem)
   ];
+
+  environment.systemPackages = with pkgs; [ kmonad ];
+  services.kmonad = {
+    enable = true;
+    keyboards = {
+      frameworkKeyboard = {
+        device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+        config = 
+  # environment.systemPackages = with pkgs; [ kanata ];
+  # services.kanata = {
+  #   enable = true;
+  #   keyboards.frameworkKeyboard.config = ''
+      (defcfg
+        input  (device-file "/dev/input/by-id/usb-04d9_daskeyboard-event-kbd")
+        output (uinput-sink
+                "framework keyboard's kmonad out" ;; name of the created keyboard
+                "....") ;; additional, environment-specific, information
+        fallthrough true
+        allow-cmd false ;; maybe change later?
+
+        ;; aliases
+        
+        (defalias
+          agr (layer-toggle altgr)
+        )
+
+        ;; srcs and layers
+
+        (defsrc
+          esc   f1   f2   f3   f4   f5   f6   f7   f8   f9   f10   f11   f12   del
+          grb    1    2    3    4    5    6    7    8    9     0     -     =   bspc
+          tab    q    w    e    r    t    y    u    i    o     p     [     ]    \  
+          caps   a    s    d    f    g    h    j    k    l    ;      '       ret
+          lsft   z    x    c    v    b    n    m    ,    .    /     up  rsft  
+          lctl wkup lmet lalt      spc            ralt rctl  left  down right
+        )
+        (deflayer qwerty
+          esc   f1   f2   f3   f4   f5   f6   f7   f8   f9   f10   f11   f12   del
+          grb    1    2    3    4    5    6    7    8    9     0     -     =   bspc
+          tab    q    w    e    r    t    y    u    i    o     p     [     ]    \  
+          caps   a    s    d    f    g    h    j    k    l    ;      '       ret
+          lsft   z    x    c    v    b    n    m    ,    .    /     up  rsft  
+          lctl wkup lmet lalt      spc            agr rwin  left  down right
+        )
+        (deflayer altgr
+          -     -    -    -    -    -    -    -    -    -    -     -     -     -   
+          -      -    -    -    -    -    -    -    -    -     -     -     -   -   
+          -      -    -    -    -    -    -    home pgdn pgup  end   -     -    -   
+          -      -    -    -    -    -    -    left down up   right  -       -   
+          -      -    -    -    -    -    -    -    -    -    -     -   -     
+          -    -    -    -         -              -    -     -     -    -    
+        )
+      )
+    '';
+  };
 }
